e6ce66d10bd02787e681bacca2b292a6ad9efe81
Shana <shana@MacBook-Air-de-Shana.local>
2024-12-10 15:50:24 +0100

    mise a jour

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename='/association'>
       <div className="App">
         {/* Définition des routes */}
         <Routes>
